f6f253aae3f84f6ff46c68d34d40b512b32c8fc3
zhanglei <357733652@qq.com>
2020-08-26 22:51:49 +0800

    由 设备中心 =》 mqtt 代理 =》 mqtt客户端

--- Controller/DeviceCenterController.php
+++ Controller/DeviceCenterController.php
@@ -2,21 +2,48 @@
 
 namespace Controller;
 
+use Pendant\Common\MQTTProxyTool;
 use Pendant\MQTT\DeviceCenterHandle;
+use Pendant\MQTT\MQTTProxyPool;
+use Pendant\SwooleSysSocket;
 use Structural\System\MQTTProxyProtocolStruct;
 
 class DeviceCenterController extends DeviceCenterHandle
 {
+    public $tool;
+
+    public function __construct()
+    {
+        // TODO: Implement onConnect() method.
+        $this->tool = MQTTProxyTool::getInstance();
+    }
 
     public function onConnect()
     {
-        // TODO: Implement onConnect() method.
+
     }
 
     public function onReceive(MQTTProxyProtocolStruct $protocol)
     {
         // TODO: Implement onReceive() method.
         //
+        if ($protocol->type == MQTTProxyProtocolStruct::DEVICE_CENTER_CLIENT) {
+            $client_id = $protocol->client_id;
+            $payload = ($protocol->payload);
+            $token = md5(uniqid());
+            $pack_protocol = new MQTTProxyProtocolStruct();
+            $pack_protocol->type = MQTTProxyProtocolStruct::MQTT_PROXY;
+            $pack_protocol->client_id = $protocol->client_id;
+            $pack_protocol->mqtt_type = MQTTProxyProtocolStruct::OnPublishMessage;
+            $pack_protocol->message_no = MQTTProxyProtocolStruct::RETURN_OK;
+            $payload["topic"] = $client_id."/request/".$token;
+            $pack_protocol->payload = json_encode($payload);
+
+            SwooleSysSocket::$swoole_server->send(MQTTProxyPool::getInstance()->getProxy(),
+                $this->tool->pack($pack_protocol));
+        } else {
+
+        }
     }
 
     public function onClose()
--- device_center_client.php
+++ device_center_client.php
@@ -4,4 +4,4 @@ include_once "../device-center-framework/autoload.php";
 
 $instance = Pendant\MQTT\DeviceCenterClient::getInstance();
 $instance->setPort(9800);
-$instance->publish("test", "111111", "hahahaha");
+$instance->publish("lei", "111111", "hahahaha");
